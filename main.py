2085073ddcb8fed8c610a61fae333932948dd857
Oderjunkie <58880677+Oderjunkie@users.noreply.github.com>
2020-10-29 22:31:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -29,9 +29,10 @@ MATCHMAKERS = {
     ]
 }
 
-area = 'ASIA'
-ipout = MATCHMAKERS[area]
-ipout = ipout[random.randint(0,len(ipout)-1)]
+#area = 'ASIA'
+#ipout = MATCHMAKERS[area]
+#ipout = ipout[random.randint(0,len(ipout)-1)]
+ipout = '192.168.100.16'
 print(ipout)
 ipaddr = '192.168.100.139'
 port = 22023
@@ -41,36 +42,43 @@ getnonce = None
 setnonce = None
 sockin.bind((ipaddr, port))
 parseerrors = 0
+server = create.Server(sockin)
 def startserver():
     global port
     global ipout
     global ipaddr
+    global server
     global sockin
     global sockout
+    global parseerrors
     while True:
         datain, addrin = sockin.recvfrom(4096)
         if not datain:
             continue
-        sockout.sendto(datain, (ipout, port))
-        dataout, addrout = sockout.recvfrom(4096)
+        #dataout, addrout = sockout.recvfrom(4096)
         intin, intout = '{}', '{}'
-        f = open('logother.log', 'a')
+        f = open('420log.log', 'a')
         try:
             intin = parse.packet(datain, 'client')
             f.write('[C] <<== {}\r\n'.format(str(intin)))
+            print('[C] <<== {}'.format(str(intin)))
+            pack = server.packet(intin, addrin)
+            #intout = parse.packet(pack, 'server')
+            #f.write('[S] ==>> [{}]\n'.format(pack.hex()))
+            #print(intin)
         except Exception:
             parseerrors += 1
             f.write('[C] <<== [{}]\r\n'.format(datain.hex()))
-        try:
-            intout = parse.packet(dataout, 'server')
-            f.write('[S] ==>> {}\r\n'.format(str(intout)))
-            if intout['type']=='redirect':
-                ipout, port = intout['data']
-        except Exception:
-            parseerrors += 1
-            f.write('[S] ==>> [{}]\r\n'.format(dataout.hex()))
+        #try:
+        #    intout = parse.packet(dataout, 'server')
+        #    f.write('[S] ==>> {}\r\n'.format(str(intout)))
+        #    if intout['type']=='redirect':
+        #        ipout, port = intout['data']
+        #except Exception:
+        #    parseerrors += 1
+        #    f.write('[S] ==>> [{}]\r\n'.format(dataout.hex()))
         f.close()
-        sockin.sendto(dataout, addrin)
+        #sockin.sendto(dataout, addrin)
         #out = parse.packet(data)
         #getnonce = out['nonce']
         #setnonce = create.packet(getnonce, out, sock, database)
