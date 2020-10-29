b3e9fd8ee7ba1de95c95d21c53782d4dc435a644
Oderjunkie <58880677+Oderjunkie@users.noreply.github.com>
2020-10-29 17:08:00 +0300

    v1.0.0 prep

--- reliparse.py
+++ reliparse.py
@@ -1,11 +1,11 @@
-def reliparse(spig):
-    nonce, length, opcode = spig.wet('@@1')
+from converting import bytetoip
+def reliparse(spig, cliserv):
+    nonce, length, opcode = spig.wet('>@<@!')
     if opcode==0:
         # "Host Game"
-        length = spig.drip('packed')
-        version, maxPlay, language, mapId, plSpMod, crLiMod,\
-        imLiMod, kiCool, common, long, short, emerg, impCount,\
-        killDist, disc, voting, isDefault = spig.wet('!!$!ffff!!!4!!44?')
+        length, version, maxPlay, language, mapId, plSpMod, crLiMod,\
+        imLiMod, kiCool, common, long, short, [emerg,], impCount,\
+        killDist, disc, voting, isDefault = spig.wet('p!!$!ffff!!!4!!44?')
         emergency, confirmEjects, visualTasks,\
         anonymusVoting, taskBarUpdates = [20, True, True, False, 0]
         if version>1:
@@ -14,6 +14,8 @@ def reliparse(spig):
                 confirmEjects, visualTasks = spig.wet('??')
                 if version>3:
                     anonymusVoting, taskBarUpdates = spig.wet('?!')
+        spig.dry()
+        assert spig.feed()==None
         return {'type':'hostgame', 'nonce':nonce, 'data':(version, maxPlay, language, mapId, plSpMod, crLiMod, imLiMod,\
                                                           kiCool, common, long, short, emerg, impCount, killDist,\
                                                           disc, voting, isDefault, emergency, confirmEjects,\
@@ -21,40 +23,30 @@ def reliparse(spig):
     elif opcode==1:
         # "Join Game"
         code, mapown = spig.wet('c!')
+        assert spig.feed()==None
         return {'type':'joingame', 'nonce':nonce, 'data':code}
     elif opcode==16:
-        #length = spig.drip('packed')
-        version = spig.drip('byte')
-        maxPlay = spig.drip('byte')
-        language = spig.drip('uint32')
-        mapId = spig.drip('byte')
-        plSpMod = spig.drip('float32')
-        crLiMod = spig.drip('float32')
-        imLiMod = spig.drip('float32')
-        kiCool = spig.drip('float32')
-        common = spig.drip('byte')
-        long = spig.drip('byte')
-        short = spig.drip('byte')
-        emerg = spig.drip('int32')
-        impCount = spig.drip('byte')
-        killDist = spig.drip('byte')
-        disc = spig.drip('int32')
-        voting = spig.drip('int32')
-        isDefault = spig.drip('boolean')
-        emergency = 20
-        confirmEjects = True
-        visualTasks = True
-        anonymusVoting = False
-        taskBarUpdates = 0
-        if version>1:
-            emergency = spig.drip('byte')
-            if version>2:
-                confirmEjects = spig.drip('boolean')
-                visualTasks = spig.drip('boolean')
-                if version>3:
-                    anonymusVoting = spig.drip('boolean')
-                    taskBarUpdates = spig.drip('byte')
-        return {'type':'getgamelist', 'nonce':nonce, 'data':(version, maxPlay, language, mapId, plSpMod, crLiMod, imLiMod,\
-                                                          kiCool, common, long, short, emerg, impCount, killDist,\
-                                                          disc, voting, isDefault, emergency, confirmEjects,\
-                                                          visualTasks, anonymusVoting, taskBarUpdates)}
+        # "Game List Request" HOW DOES THIS JANK FORMAT EVEN WORKKKKK
+        if cliserv=='client':
+            length, version, language, mapId, impCount = spig.wet('px!x$!xxxxxxxxxxxxxxxxxxxxxxx!xxxxxxxxxx')
+            emergency, confirmEjects, visualTasks,\
+            anonymusVoting, taskBarUpdates = [20, True, True, False, 0]
+            if version>1:
+                emergency = spig.wet('!')
+                if version>2:
+                    confirmEjects, visualTasks = spig.wet('??')
+                    if version>3:
+                        anonymusVoting, taskBarUpdates = spig.wet('?!')
+            spig.dry()
+            assert spig.feed()==None
+            return {'type':'gamelistrequest', 'nonce':nonce, 'data':(language, mapId, impCount)}
+        elif cliserv=='server':
+            return {'type':'gamelist', 'nonce':nonce, 'data':None}
+    elif opcode==13:
+        # "Redirect"
+        ip, port = spig.wet('i@')
+        assert spig.feed()==None
+        return {'type':'redirect', 'nonce':nonce, 'data':(ip, port)}
+    else:
+        print(opcode)
+        assert False # what
