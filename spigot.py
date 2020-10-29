75e0fc2568bfc89167128106f48a57f03685c861
Oderjunkie <58880677+Oderjunkie@users.noreply.github.com>
2020-10-29 17:08:58 +0300

    v1.0.0 prep

--- spigot.py
+++ spigot.py
@@ -1,88 +1,52 @@
-import struct
-from converting import unpack, bytetocodev1, bytetocodev2
-class Spigot:
-    def __init__(self):
-        self.bytearr = []
-    def feed(self, newarr=b''):
-        if not self.bytearr:
-            self.bytearr = bytearray(newarr)
-            return None
-        else:
-            return self.bytearr.pop(0)
-    def dry(self):
-        self.bytearr = bytearray(b'')
-    def drip_help(self, form):
-        if form[-1] in 'xcbB?':
-            return struct.unpack(form, bytes([self.feed()]))[0]
-        if form[-1] in 'hH':
-            return struct.unpack(form, bytes([self.feed(),self.feed()]))[0]
-        if form[-1] in 'iIlLf':
-            return struct.unpack(form, bytes([self.feed(),self.feed(),self.feed(),self.feed()]))[0]
-        if form[-1] in 'qQd':
-            return struct.unpack(form, bytes([self.feed(),self.feed(),self.feed(),self.feed(),self.feed(),self.feed(),self.feed(),self.feed()]))[0]
-    def drip(self, type, endian='<'):
-        form = endian
-        #print(type)
-        if type not in ['string', 'packed', 'codev1', 'codev2', 'code']:
-            if type=='boolean': form += '?'                         #   Boolean
-            if type in ['int8']: form += 'b'                        #   1
-            if type in ['byte', 'uint8']: form += 'B'               #   u1
-            if type in ['short', 'int16']: form += 'h'              #   2
-            if type in ['ushort', 'uint16']: form += 'H'            #   u2
-            if type in ['int', 'int32']: form += 'i'                #   4
-            if type in ['uint', 'uint32']: form += 'I'              #   u4
-            if type=='long': form += 'q'                            #   8
-            if type=='ulong': form += 'Q'                           #   u8
-            if type in ['float', 'float32']: form += 'f'            #   4
-            if type=='double': form += 'd'                          #   8
-            return self.drip_help(form)
-        else:
-            if type=='string':
-                len = self.feed()
-                str = b''
-                while len>0:
-                    str += bytes([self.feed()])
-                    len -= 1
-                return str.decode()
-            if type=='packed':
-                return unpack(self)
-            if type=='codev1':
-                return bytetocodev1(bytes([self.feed(), self.feed(), self.feed(), self.feed()]))
-            if type=='codev2':
-                return bytetocodev2(bytes([self.feed(), self.feed(), self.feed(), self.feed()]))
-            if type=='code':
-                code = [self.feed(), self.feed(), self.feed(), self.feed()]
-                if 0x20<code[0]<0x70 and\
-                   0x20<code[1]<0x70 and\
-                   0x20<code[2]<0x70 and\
-                   0x20<code[3]<0x70:
-                    return bytetocodev1(bytes(code))
-                return bytetocodev2(bytes(code))
-    def wet(self, cusform):
-        out = []
-        form = cusform
-        endian = '@'
-        if cusform[0] in '<>':
-            endian = cusform[0]
-            form = cusform[1:]
-        for char in form:
-            #print('a: ', char)
-            if char=='x': self.feed()
-            elif char=='?': out.append(self.drip('boolean', endian))
-            elif char=='1': out.append(self.drip('int8', endian))
-            elif char=='!': out.append(self.drip('uint8', endian))
-            elif char=='2': out.append(self.drip('int16', endian))
-            elif char=='@': out.append(self.drip('uint16', endian))
-            elif char=='4': out.append(self.drip('int32', endian))
-            elif char=='$': out.append(self.drip('uint32', endian))
-            elif char=='8': out.append(self.drip('long', endian))
-            elif char=='*': out.append(self.drip('ulong', endian))
-            elif char=='4': out.append(self.drip('uint16', endian))
-            elif char=='f': out.append(self.drip('float', endian))
-            elif char=='d': out.append(self.drip('double', endian))
-            elif char=='s': out.append(self.drip('string'))
-            elif char=='p': out.append(self.drip('packed'))
-            elif char=='c': out.append(self.drip('code'))
-            elif char=='v': out.append(self.drip('codev2'))
-            elif char=='V': out.append(self.drip('codev1'))
-        return out
+from converting import bytetoip
+def reliparse(spig, cliserv):
+    nonce, length, opcode = spig.wet('>@<@!')
+    if opcode==0:
+        # "Host Game"
+        length, version, maxPlay, language, mapId, plSpMod, crLiMod,\
+        imLiMod, kiCool, common, long, short, [emerg,], impCount,\
+        killDist, disc, voting, isDefault = spig.wet('p!!$!ffff!!!4!!44?')
+        emergency, confirmEjects, visualTasks,\
+        anonymusVoting, taskBarUpdates = [20, True, True, False, 0]
+        if version>1:
+            emergency = spig.wet('!')
+            if version>2:
+                confirmEjects, visualTasks = spig.wet('??')
+                if version>3:
+                    anonymusVoting, taskBarUpdates = spig.wet('?!')
+        spig.dry()
+        assert spig.feed()==None
+        return {'type':'hostgame', 'nonce':nonce, 'data':(version, maxPlay, language, mapId, plSpMod, crLiMod, imLiMod,\
+                                                          kiCool, common, long, short, emerg, impCount, killDist,\
+                                                          disc, voting, isDefault, emergency, confirmEjects,\
+                                                          visualTasks, anonymusVoting, taskBarUpdates)}
+    elif opcode==1:
+        # "Join Game"
+        code, mapown = spig.wet('c!')
+        assert spig.feed()==None
+        return {'type':'joingame', 'nonce':nonce, 'data':code}
+    elif opcode==16:
+        # "Game List Request" HOW DOES THIS JANK FORMAT EVEN WORKKKKK
+        if cliserv=='client':
+            length, version, language, mapId, impCount = spig.wet('px!x$!xxxxxxxxxxxxxxxxxxxxxxx!xxxxxxxxxx')
+            emergency, confirmEjects, visualTasks,\
+            anonymusVoting, taskBarUpdates = [20, True, True, False, 0]
+            if version>1:
+                emergency = spig.wet('!')
+                if version>2:
+                    confirmEjects, visualTasks = spig.wet('??')
+                    if version>3:
+                        anonymusVoting, taskBarUpdates = spig.wet('?!')
+            spig.dry()
+            assert spig.feed()==None
+            return {'type':'gamelistrequest', 'nonce':nonce, 'data':(language, mapId, impCount)}
+        elif cliserv=='server':
+            return {'type':'gamelist', 'nonce':nonce, 'data':None}
+    elif opcode==13:
+        # "Redirect"
+        ip, port = spig.wet('i@')
+        assert spig.feed()==None
+        return {'type':'redirect', 'nonce':nonce, 'data':(ip, port)}
+    else:
+        print(opcode)
+        assert False # what
