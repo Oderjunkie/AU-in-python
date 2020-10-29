41da6cdf873bd9b8790707ecbd58ddec02fb8adb
Oderjunkie <58880677+Oderjunkie@users.noreply.github.com>
2020-10-29 17:09:25 +0300

    v1.0.0 prep

--- unreliparse.py
+++ unreliparse.py
@@ -1,1 +1,6 @@
-import struct
+def unreliparse(spig, clientserv):
+    length, opcode = spig.wet('2!')
+    if opcode==14:
+        if clientserv=='server':
+            return {'type':'masterserverlist'}
+    assert False # Still don't support [MOST] unreliable packets...
